9dd575616b281522398fd601586e8ee205217f09
Shadic78 <carlosjosue7889@gmail.com>
2021-11-06 12:15:33 -0600

    Refactors and upgrades to CrudTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { Login } from './auth/Login';
+import { exampleData } from './util/exampleData';
 import CrudTable from './components/CrudTable';
 
 const App = () => {
+  const handleUpdate = (updatedData, updatedObject) => {
+    console.log('Updated object:');
+    console.log(updatedObject);
+    console.log('Updated all data:');
+    console.log(updatedData);
+  };
+
+  const handleReset = (data) => {
+    console.log('Reset data');
+    console.log(data);
+  };
+
   return (
     <ChakraProvider>
       <Login />
-      <CrudTable />
+      <CrudTable 
+        handleChange={handleUpdate}
+        handleReset={handleReset}
+        customData={exampleData}  
+      />
     </ChakraProvider>
   );
 };
--- src/components/CrudTable/index.tsx
+++ src/components/CrudTable/index.tsx
@@ -1,60 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import Table from './Table/Table';
 import { Styles } from './styles';
-import {parseData} from '../../util/tableDataParser';
+import { parseData } from '../../util/tableDataParser';
 
-const CrudTable = () => {
-  const customData = [
-    {
-      name: 'Carlos',
-      lastName: 'Chan',
-      age: 21,
-      email: 'carlos@gmail.com'
-    },
-    {
-      name: 'Emmanuel',
-      lastName: 'Chable',
-      age: 22,
-      email: 'emman@gmail.com'
-    },
-    {
-      name: 'Nicolas',
-      lastName: 'Canul',
-      age: 21,
-      email: 'nicolas@gmail.com'
-    },
-    {
-      name: 'Eusebio',
-      lastName: 'Ajax',
-      age: 21,
-      email: 'eusebio@gmail.com'
-    }
-  ];
-  const [data, setData] = useState(customData)
+const CrudTable = ({customData, handleChange, handleReset}) => {
+  const [data, setData] = useState(customData as Array<any>)
   const [originalData] = useState(data)
   const [skipPageReset, setSkipPageReset] = useState(false)
   const [myColumns] = parseData(customData);
   
   const updateMyData = (rowIndex: number, columnId: number, value: string) => {
     setSkipPageReset(true)
-    setData((old: any) =>
-      old.map((row: number, index: number) => {
-        if (index === rowIndex) {
-          return {
-            ...old[rowIndex],
-            [columnId]: value,
-          }
-        }
-        return row
-      })
-    )
+    const oldObject = data[rowIndex];
+    const updatedObject = {...oldObject, [columnId]: value};
+    const updatedData = data.map((row: number, index: number) => {
+      if (index === rowIndex) {
+        return updatedObject;
+      }
+      return row;
+    });
+    setData(updatedData);
+    handleChange(updatedData, updatedObject);
   }
 
   useEffect(() => {
     setSkipPageReset(false)
   }, [data])  
 
-  const resetData = () => setData(originalData)
+  const resetData = () => {
+    setData(originalData);
+    handleReset(originalData);
+  }
 
   return (
     <Styles>
