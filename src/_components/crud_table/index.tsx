5332db713cab227e48fe6563897e280350949d13
HikingCarrot7 <ricardoibarra2044@gmail.com>
2021-11-14 11:50:28 -0600

    fixed table data model  not updated when item gets deleted

--- src/_components/crud_table/index.tsx
+++ src/_components/crud_table/index.tsx
@@ -1,53 +1,79 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { parseData } from 'utils/tableDataParser';
 import { DataTable } from './table/Table';
 
-export const CrudTable = ({ populateWith, handleUpdate, handleDelete }) => {
-  const [data, setData] = useState(populateWith as Array<any>);
+export interface CrudTableProps<T> {
+  populateWith: T[];
+  askConfirmationOnDelete?: boolean;
+  onUpdateDataState: React.Dispatch<React.SetStateAction<any[]>>;
+  handleUpdate: (updatedData, updatedObject) => Promise<boolean>;
+  handleDelete: (updatedData, updatedObject) => Promise<boolean>;
+}
+
+export const CrudTable = <T extends unknown>({
+  populateWith,
+  onUpdateDataState,
+  handleUpdate,
+  handleDelete,
+  askConfirmationOnDelete = true,
+}: CrudTableProps<T>) => {
   const [skipPageReset, setSkipPageReset] = useState(false);
   const [columns, setColumns] = useState([]);
 
-  useEffect(() => {
-    setData(populateWith);
-  }, [populateWith]);
+  const deleteRowCallback = useCallback(
+    (updatedData, deletedData) => {
+      const deleteItem = () => {
+        handleDelete(updatedData, deletedData)
+          .then(() => {
+            onUpdateDataState(updatedData);
+          })
+          .catch((err) => {});
+      };
+
+      if (askConfirmationOnDelete) {
+        if (window.confirm('¿Estás seguro que quiere eliminar esta entrada?')) {
+          deleteItem();
+        }
+      } else {
+        deleteItem();
+      }
+    },
+    [askConfirmationOnDelete, handleDelete, onUpdateDataState]
+  );
 
   useEffect(() => {
     setSkipPageReset(false);
-    setColumns(parseData(data, deleteData));
-  }, [data]);
-
-  const deleteData = (updatedData, deletedData, rowIndex) => {
-    handleDelete(updatedData, deletedData).then(() => {
-      setData(updatedData);
-    });
-  };
+    setColumns(parseData(populateWith, deleteRowCallback));
+  }, [populateWith, deleteRowCallback]);
 
   const updateData = (rowIndex: number, columnId: string, value: any) => {
     setSkipPageReset(true);
-    const oldObject = data[rowIndex];
+    const oldObject = populateWith[rowIndex];
 
     const isDataUnchanged = oldObject[columnId] === value;
 
     if (isDataUnchanged) return false;
 
-    const updatedObject = { ...oldObject, [columnId]: value };
+    const updatedObject = { ...(oldObject as any), [columnId]: value };
 
-    const updatedData = data.map((row: number, index: number) => {
+    const updatedData = populateWith.map((item: T, index: number) => {
       if (index === rowIndex) {
         return updatedObject;
       }
 
-      return row;
+      return item;
     });
 
     handleUpdate(updatedData, updatedObject)
       .then(() => {
-        setData(updatedData);
+        onUpdateDataState(updatedData);
       })
       .catch((err) => {});
 
     return true;
   };
 
-  return <DataTable {...{ columns, data, updateData, skipPageReset }} />;
+  return (
+    <DataTable {...{ columns, populateWith, updateData, skipPageReset }} />
+  );
 };
